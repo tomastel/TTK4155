1a147027d2e82197fcd297951d7243272a37d0b8
Tomas Nils Tellier <tomasnt@ntnu.no>
2022-10-13 18:34:38 +0200

    Lab 6 finished?

---
+++ Node_1/LAB_6/ttk_4155_byggern_lab/ADC/adc_drv.h
@@ -0,0 +1,62 @@
+/*
+ * IncFile1.h
+ *
+ * Created: 15.09.2022 12:41:44
+ *  Author: khuongh
+ */ 
+
+
+#ifndef _ADC_DRV_H_
+#define _ADC_DRV_H_
+
+
+#include "../MISC/common_includes.h"
+
+#define LEFT_SLIDER_CHANNEL 0
+#define RIGHT_SLIDER_CHANNEL 1
+#define JOYSTICK_Y_CHANNEL 2
+#define JOYSTICK_X_CHANNEL 3
+#define ADC_EXT_CLK_SGN_PIN PD5
+#define ADC_MAX 255
+#define ADC_JOYSTICK_THRESHOLD 70 //Changed from 3
+
+typedef struct {
+	int8_t X;
+	int8_t Y;
+}pos_t;
+
+typedef enum {
+	LEFT,
+	RIGHT,
+	UP,
+	DOWN,
+	NEUTRAL,
+	UP_RIGHT,
+	UP_LEFT,
+	DOWN_RIGHT,
+	DOWN_LEFT,
+}dir_t;
+
+typedef struct {
+	pos_t position;
+	dir_t direction;
+}fun_stick_t;
+
+fun_stick_t adc_drv_fun_stick_get();
+
+bool adc_drv_joystick_update();
+
+void adc_drv_init();
+
+uint8_t adc_read(uint8_t channel); //volatile
+
+void adc_calibrate();
+
+pos_t pos_read();
+
+dir_t dir_read(pos_t positions);
+
+int8_t adc_conv_js_val(uint8_t raw_value, uint8_t offset_value);
+
+
+#endif /* INCFILE1_H_ */
---
+++ Node_1/LAB_6/ttk_4155_byggern_lab/OLED/menu.h
@@ -0,0 +1,64 @@
+/*
+ * menu.h
+ *
+ * Created: 29.09.2022 12:27:21
+ *  Author: tomasnt
+ */ 
+
+
+#ifndef MENU_H_
+#define MENU_H_
+
+#define FIRST_LINE_MENU 1
+#define LAST_LINE_MENU 7
+#define OPTIONS_MAIN_MENU 7
+#define MAX_SUBMENUS 7
+#define MENU_OPTION_MAX_STRING_LENGTH 24
+
+#include <string.h>
+#include "../MISC/common_includes.h"
+#include "oled_drv.h"
+#include "../ADC/adc_drv.h"
+
+
+
+typedef struct
+{
+	char option_name[MENU_OPTION_MAX_STRING_LENGTH];
+	void(*callback)(void*);
+	void *callback_parameter;
+	
+}option_struct;
+
+typedef struct
+{
+	char title[MENU_OPTION_MAX_STRING_LENGTH];
+	option_struct submenu[MAX_SUBMENUS];
+	int options;
+	
+}menu_struct;
+
+
+void this_end();
+
+void that_end();
+
+void print_menu_page();
+
+void update_menu(void *menu_page);
+
+void menu_navigate();
+
+void print_arrow();
+
+void clear_arrow();
+
+void move_arrow_up();
+
+void move_arrow_down();
+
+void enter();
+
+void menu_init();
+
+#endif /* MENU_H_ */
